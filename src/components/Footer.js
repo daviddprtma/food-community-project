db8b88908929c675866f91f10c609fe64ebe4a89
daviddprtma <davidchristianpratama@gmail.com>
2023-01-26 19:38:52 +0700

    quest 4 done

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <div>
+         <p>David Pratama Foodary © 2023</p>
+      </div>
+    );
+  }
+  export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,43 @@
+export const my_recipe = {
+  "recipe_1": {
+    "name": 'Chirashi Don',
+    "preparation_time": '30 minutes',
+    "servings": '2',
+    "image":"/img/recipe.jpg",
+    "ingredients": [
+      '1/2 cup rice',
+      '1/2 cup water',
+      '2 eggs',
+      '300g of salmon, tuna and salmon roe',
+    ],
+    "instructions": [
+      'Cook rice and water in a rice cooker',
+      'Fry eggs in a pan',
+      'Serve rice, eggs and fish on a plate',
+    ],
+ 
+  },
+    "recipe_2": {
+    "name": 'Fried rice ',
+    "preparation_time": '20 minutes',
+    "servings": '4',
+    "image":'/img/nasigoreng.jpg',
+    "ingredients": [
+      '1 Plate white rice',
+      'Garlic 2 cloves, finely chopped',
+      'Sweet soy sauce or salty soy sauce according to taste',
+      'Chicken sausage 1 piece, thinly sliced',
+    ],
+    "instructions": [
+      'Prepare a frying pan over medium heat, pour margarine or cooking oil.',
+      'Add the finely chopped garlic and chives. Sauté until fragrant or until golden in color.',
+      'Add sausage and 1 chicken egg. Saute for a minute.',
+      'Add the ground spices and rice. Stir until well mixed.',
+      'Pour in sweet soy sauce, chili sauce, oyster sauce, salt and broth powder. Stir until the color of the rice changes evenly.',
+      'Simple and delicious plain fried rice is ready to be served.'
+    ],
+ 
+  },
+
+  
+}
